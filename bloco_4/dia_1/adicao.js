f0c472bb31bd1d70c9ff887657a685b7cf45535e
lcgoandete <lcgoandete@hotmail.com>
2020-11-25 16:55:27 -0300

    concluido exercicio 1 Adição, Subtração, Multiplicação, Divisão e Módulo

---
+++ bloco_4/dia_1/adicao.js
@@ -0,0 +1,5 @@
+function subtracao (a , b) {
+  return a + b;
+}
+
+console.log(subtracao(14, 15));
---
+++ bloco_4/dia_1/divisao.js
@@ -0,0 +1,8 @@
+function modulo (a , b) {
+  if (b === 0) {
+    return 'Não é permitida divisão por zero.';
+  }
+  return a / b;
+}
+
+console.log(modulo(17, 3));
