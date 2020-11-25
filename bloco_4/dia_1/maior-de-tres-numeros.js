57910e622d8b10bf7899ada2b0a36d03482dfeae
lcgoandete <lcgoandete@hotmail.com>
2020-11-25 18:53:01 -0300

    concluido exercicio 6 - xadres

--- bloco_4/dia_1/maior-de-tres-numeros.js
+++ bloco_4/dia_1/maior-de-tres-numeros.js
@@ -1,6 +1,3 @@
-// Faça um programa que retorne o maior de três números.
-// Defina no começo do programa três variáveis com os valores que serão comparados.
-
 let a = 4;
 let b = 5;
 let c = 3;
---
+++ bloco_4/dia_1/xadres.js
@@ -0,0 +1,22 @@
+const pecas = ['rei', 'rainha', 'bispo', 'cavalo', 'torre', 'peão'];
+const movimentos = [
+  'Pode ser movimentado em qualquer direção do tabuleiro, mas apenas de casa em casa.',
+  'Sem restrições, a Rainha tem livre movimentação no jogo na horizontal, vertical e diagonais.',
+  'Sem restrição de número de casas, mas somente no sentido diagonal.',
+  'Realiza movimentos em “L”, ou seja, duas casas em um sentido e uma casa em sentido perpendicular àquele, em qualquer direção.',
+  'Pode correr sem restrição de número de casas e em todas as direções (frente, trás, direita, esquerda).',
+  'Pode apenas realizar movimentos frontais, de forma que o primeiro movimento de cada peão abranja até duas casas, e os demais se limitam a uma casa à frente. O ataque do peão sempre ocorre na diagonal.'
+];
+
+function escolherPeca (pecaEscolhida) {
+  pecaEscolhida = pecaEscolhida.toLowerCase();
+  
+  for (let i = 0; i < pecas.length; i++) {
+    if (pecaEscolhida === pecas[i]) {
+      return pecas[i] + ' -> ' + movimentos[i];
+    } 
+  }
+  return 'Erro - Peça inválida';
+}
+
+console.log(escolherPeca('RAINHA'));
