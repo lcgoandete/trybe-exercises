6544142d238471475f9165b235e54a9521517802
lcgoandete <lcgoandete@hotmail.com>
2020-12-04 18:48:14 -0300

    Exercicio 1 - criar dias do calendario

--- bloco_5/dia_3/README.md
+++ bloco_5/dia_3/README.md
@@ -5,7 +5,7 @@ Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex
 Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
 Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
 
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 -=-=-=-=-=-=-=-=-=-=-
 
---
+++ bloco_5/dia_3/script.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+  createCalendarDays();
+  
+}
+
+function createCalendarDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const days = document.querySelector('#days');
+  
+  for (const day of dezDaysList) {
+    let dayTemp = document.createElement('li');
+    dayTemp.innerText = day;
+    dayTemp.className = 'day';
+    
+    switch (day) {
+      case 4:
+        dayTemp.className = 'day friday';
+        break;
+      case 11:
+        dayTemp.className = 'day friday';
+        break;
+      case 18:
+        dayTemp.className = 'day friday';
+        break;
+      case 24:
+        dayTemp.className = 'day holiday';
+        break;
+      case 25:
+        dayTemp.className = 'day holiday friday';
+        break;
+      case 31:
+        dayTemp.className = 'day holiday';
+        break;
+    }
+    days.appendChild(dayTemp);
+  }
+}
