d79bfa01f579d8e052baaccda15919c1f448e747
lcgoandete <lcgoandete@hotmail.com>
2021-01-06 19:06:12 -0300

    completando exercicio 1

---
+++ bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,10 @@
+estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina","São Paulo","Sergipe","Tocantins"];
+
+const uf = document.querySelector('#estado');
+
+for (const estado of estados) {
+  let option = document.createElement('option');
+  option.innerText = estado;
+  uf.appendChild(option);
+}
+
